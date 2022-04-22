53a5b8bced1851a5e758055fb825681327995381
Den <52295780+josserden@users.noreply.github.com>
2022-04-22 10:58:36 +0300

    fix

---
+++ src/js/getRefs.js
@@ -0,0 +1,7 @@
+export const getrefs = () => {
+  return {
+    formSearch: document.querySelector('.js-search'),
+    modalForm: document.querySelector('.js-modal-form'),
+    contactList: document.querySelector('.js-contact-list'),
+  };
+};
