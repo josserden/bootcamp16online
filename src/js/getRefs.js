472d616557b76e2802f0322e968e3b5326e606be
Den <52295780+josserden@users.noreply.github.com>
2022-04-22 10:58:58 +0300

    fix

--- src/js/getRefs.js
+++ src/js/getRefs.js
@@ -1,4 +1,4 @@
-export const getrefs = () => {
+export const getRefs = () => {
   return {
     formSearch: document.querySelector('.js-search'),
     modalForm: document.querySelector('.js-modal-form'),
