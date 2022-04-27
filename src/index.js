ad34b0f94eaae0a5fc22362424107393ad0e8a43
Den <52295780+josserden@users.noreply.github.com>
2022-04-27 21:03:55 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
 import './sass/main.scss';
-
-fetch('https://httpstat.us/503')
-  .then(function (response) {
-    console.log('200 - ok');
-  })
-  .catch(function (error) {
-    console.log('Error : ' + error);
-  });
