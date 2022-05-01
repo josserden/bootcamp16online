b2cd6b7ebf2e6741753464a9f80041a3a192c18d
Den <52295780+josserden@users.noreply.github.com>
2022-05-01 21:28:01 +0300

    create

--- src/js/getRefs.js
+++
@@ -1,7 +0,0 @@
-export const getRefs = () => {
-  return {
-    commentContainer: document.querySelector('.comments-container'),
-    loadMoreBtn: document.querySelector('.load-more-btn'),
-    backToTopBtn: document.querySelector('.back-to-top'),
-  };
-};
