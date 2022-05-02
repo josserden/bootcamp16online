1bfe6d927c5517406fc9324c87f7eb2ef8d68b41
Den <52295780+josserden@users.noreply.github.com>
2022-05-02 11:57:07 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,4 +1,39 @@
 import 'material-icons/iconfont/material-icons.css';
 import './sass/main.scss';
 
-// const BASE_URL = 'https://6251bfb67f7fa1b1ddde85d8.mockapi.io/api';
+import * as ContactService from './js/service/contacts-service';
+
+// GET => /contacts
+ContactService.fetchContacts()
+  .then(res => console.log(res.data))
+  .catch(error => console.error(error));
+
+// GET -> /contacts/:id
+// ContactService.getContact(16)
+//   .then(res => console.log(res.data))
+//   .catch(error => console.error(error));
+
+// POST -> /contacts
+// const contact = {
+//   name: 'Jimmy',
+//   email: 'jimmy@mail.com',
+//   number: '987-654-4444',
+// };
+
+// ContactService.createContact(contact)
+//   .then(res => console.log(res))
+//   .catch(error => console.error(error));
+
+// PUT -> /contacts/:id
+// const contact = {
+//   name: 'Maria',
+//   email: 'maria@mail.com',
+//   number: '987-654-9999',
+// };
+
+// ContactService.updateContact(32, contact)
+//   .then(res => console.log(res.data))
+//   .catch(error => console.error(error));
+
+// DELETE -> /contacts/:id
+// ContactService.deleteContact(26);
